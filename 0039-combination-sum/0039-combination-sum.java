ef2ba5ae05ea447534906ddae93a8cccc5b291ad
Ayush04 <152521754+Ayush12313065@users.noreply.github.com>
2026-07-20 11:44:57 +0530

    Time: 2 ms (96.61%), Space: 45.4 MB (96.43%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,20 +4,20 @@ class Solution {
         backtrack(candidates,0,target,new ArrayList<>(),list);
         return list;
     }
-    private void backtrack(int[] candidates,int index,int target,List<Integer> current,List<List<Integer>>list){
-        if(target ==0){
-            list.add(new ArrayList<> (current));
-            return ;
+
+    private void backtrack(int[] candidates,int index,int target , List<Integer> current,List<List<Integer>>list){
+        if(target == 0){
+            list.add( new ArrayList<> (current));
+            return;
         }
-        if(target<0){
+        if(target <0){
             return;
         }
 
-        for(int i =index ;i<candidates.length;i++){
+        for(int i = index;i < candidates.length;i++){
             current.add(candidates[i]);
-            backtrack(candidates,i,target - candidates[i],current,list);
-            current.remove(current.size() - 1);
+            backtrack(candidates,i, target - candidates[i] , current,list);
+            current.remove(current.size() -1);
         }
-
     }
 }
