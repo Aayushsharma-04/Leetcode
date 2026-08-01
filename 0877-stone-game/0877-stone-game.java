b1cd41ed0fc108e4a89f33a70e9edf18bdad91e9
Aayush Sharma <152521754+Aayushsharma-04@users.noreply.github.com>
2026-08-01 12:51:29 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.82%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+       return true;
+       /// logic recursion and dp works pick and pick not
+       ///prefix and suffix array also work like taking 1 from preffix and rest from suffix or vice versa
+       // subarray approach also works like take half of choices and maximum it and minimuse the other half.. 
+    }
+}
